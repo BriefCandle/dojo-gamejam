36fe000cd0f8bfc5229a5cf820ad289191216045
BriefCandle <yufanling@gmail.com>
2024-10-30 22:04:55 +0800

    update(client): update toriiUrl

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -3,5 +3,6 @@ import { createDojoConfig } from "@dojoengine/core";
 import manifest from "../dojo-starter/manifests/dev/deployment/manifest.json";
 
 export const dojoConfig = createDojoConfig({
+  toriiUrl: "http://localhost:1234",
   manifest,
 });
