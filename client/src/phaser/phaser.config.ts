b8c16803b0c22668cb7d7fb9701449c1d65b4860
BriefCandle <yufanling@gmail.com>
2024-11-01 21:14:13 +0800

    update(client): update phaser

--- client/src/phaser/phaser.config.ts
+++ client/src/phaser/phaser.config.ts
@@ -1,21 +1,27 @@
 import Phaser from "phaser";
+import { SpinePlugin } from "@esotericsoftware/spine-phaser";
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.WEBGL,
+  premultipliedAlpha: false,
   parent: "phaser-container",
   backgroundColor: "#1a103c",
   scale: {
-    mode: Phaser.Scale.ScaleModes.RESIZE,
+    mode: Phaser.Scale.ScaleModes.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     width: window.innerWidth,
     height: window.innerHeight,
   },
-  pixelArt: true,
   disableContextMenu: true,
   audio: {
     disableWebAudio: false,
   },
+  pixelArt: true,
+  roundPixels: true,
   plugins: {
-    scene: [],
+    scene: [
+      { key: "spine.SpinePlugin", plugin: SpinePlugin, mapping: "spine" },
+    ],
   },
 };
 
